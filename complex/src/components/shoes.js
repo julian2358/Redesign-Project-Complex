23b4c336f24e70934f53cb491809438618bcaad0
Josh McNeill <joshmcneill@macbook-pro.attlocal.net>
2020-06-18 18:01:11 -0400

    shoes-page

---
+++ complex/src/components/shoes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Shoes extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 id='main-header'> LATEST IN SNEAKERS</h1>
+                <p1 id='sub-header'> SOLE COLLECTOR |    BY JESSICA MCKINNEY</p1>
+
+            </div>
+        )
+    }
+}
+
+export default Shoes;
