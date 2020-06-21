5a7f4b74b577fdf9990976352ed51de39d751fdd
Josh McNeill <joshmcneill@macbook-pro.attlocal.net>
2020-06-21 14:41:21 -0400

    fixing-shoes

--- complex/src/components/shoes.js
+++ complex/src/components/shoes.js
@@ -25,47 +25,47 @@ class Shoes extends React.Component{
                     <div className="blueBox">
                         <img className="blueShoe" src="../imgs/blueShoe.jpeg" alt="Nike Gravity Shoe"/>
                         <p><span className="articleBy">SNEAKERS |</span>BY MATT WELTY</p>
-                        <a href="https://www.complex.com/sneakers/2020/06/puerto-rico-nike-air-force-1-canceled"><p>Nike Cancels 'Puerto Rico' Air Force 1s</p></a>
+                        <a href="https://www.complex.com/sneakers/2020/06/puerto-rico-nike-air-force-1-canceled"><p className="descriptionP">Nike Cancels 'Puerto Rico' Air Force 1s</p></a>
                     </div>
                     <div className="multiBox">
                         <img className="multiShoe" src="../imgs/multiShoe.jpeg" alt="Multi Colored Shoe"/>
                         <p><span className="articleBy">SNEAKERS |</span>BY RILEY JONES</p>
-                        <a href="https://www.complex.com/sneakers/off-white-nike-air-force-1-blue-unreleased"><p>Nike Athlete Mysteriously Receives Unreleased Off-White x Nike Air Force 1s</p></a>
+                        <a href="https://www.complex.com/sneakers/off-white-nike-air-force-1-blue-unreleased"><p className="descriptionP">Nike Athlete Mysteriously Receives Unreleased Off-White x Nike Air Force 1s</p></a>
                     </div>
                     <div className="redBox">
                         <img className="redShoe" src="../imgs/redShoe.jpeg" alt="Red Nike Running Shoe"/>
                         <p><span className="articleBy">SNEAKERS |</span>BY BRANDON RICHARD</p>
-                        <a href="https://solecollector.com/news/2020/06/nike-n7-sneakers-2020-releaee-date"><p>Nike Reveals This Year's Full N7 Sneaker Lineup</p></a>
+                        <a href="https://solecollector.com/news/2020/06/nike-n7-sneakers-2020-releaee-date"><p className="descriptionP">Nike Reveals This Year's Full N7 Sneaker Lineup</p></a>
                     </div>
                     <div className="blackBox">
                         <img className="blackShoe" src="../imgs/blackShoe.jpeg" alt="Black Nike Force Shoe"/>
                         <p><span className="articleBy">SOLE COLLECTOR |</span> BY VICTOR DENG</p>
-                        <a href="https://solecollector.com/news/2020/06/sean-wotherspoon-asics-gel-lyte-3-release-date"><p>Here's When Sean Wotherspoon's Atmos x Asics Collab Drops</p></a>
+                        <a href="https://solecollector.com/news/2020/06/sean-wotherspoon-asics-gel-lyte-3-release-date"><p className="descriptionP">Here's When Sean Wotherspoon's Atmos x Asics Collab Drops</p></a>
                     </div>
                     <div className="greenBox">
                         <img className="greenShoe" src="../imgs/greenShoe.jpeg" alt="Green Nike Shoe"/>
                         <p><span className="articleBy">SOLE COLLECTOR |</span>BY VICTOR DENG</p>
-                        <a href="https://solecollector.com/news/2020/06/nike-air-max-90-galaxy-release-date-cw6018-001"><p>The Nike Air Max 90 Is Going Galactic</p></a>
+                        <a href="https://solecollector.com/news/2020/06/nike-air-max-90-galaxy-release-date-cw6018-001"><p className="descriptionP">The Nike Air Max 90 Is Going Galactic</p></a>
                     </div>
                     <div className="whiteBlackRedBox">
                         <img className="whiteBlackRedShoe" src="../imgs/whiteBlackRedShoe.jpeg" alt="Nike Gravity Shoe"/>
                         <p><span className="articleBy">SOLE COLLECTOR |</span>BY VICTOR DENG</p>
-                        <a href="https://www.complex.com/sneakers/best-air-jordans-2020-so-far/"><p>The Best Air Jordans of 2020 (So Far)</p></a>
+                        <a href="https://www.complex.com/sneakers/best-air-jordans-2020-so-far/"><p className="descriptionP">The Best Air Jordans of 2020 (So Far)</p></a>
                     </div>
                     <div className="whiteRedBox">
                         <img className="whiteRedShoe" src="../imgs/whiteRedShoe.jpeg" alt="Nike Gravity Shoe"/>
                         <p><span className="articleBy">SOLE COLLECTOR |</span> BY VICTOR DENG</p>
-                        <a href="https://solecollector.com/news/2020/06/adidas-yeezy-boost-380-azure-release-date"><p>New Yeezy Boost 380 Reportedly Planned For Fall 2020</p></a>
+                        <a href="https://solecollector.com/news/2020/06/adidas-yeezy-boost-380-azure-release-date"><p className="descriptionP">New Yeezy Boost 380 Reportedly Planned For Fall 2020</p></a>
                     </div>
                     <div className="orangeBrownBox">
                         <img className="orangeBrownShoe" src="../imgs/orangeBrownShoe.jpeg" alt="Nike Gravity Shoe"/>
                         <p><span className="articleBy">SOLE COLLECTOR |</span>BY VICTOR DENG</p>
-                        <a href="https://solecollector.com/news/2020/06/dqm-nike-air-max-90-bacon-2020-release-date"><p>Is the 'Bacon' Nike Air Max 90 Finally Coming Back?</p></a>
+                        <a href="https://solecollector.com/news/2020/06/dqm-nike-air-max-90-bacon-2020-release-date"><p className="descriptionP">Is the 'Bacon' Nike Air Max 90 Finally Coming Back?</p></a>
                     </div>
                     <div className="brownBox">
                         <img className="brownShoe" src="../imgs/brownShoe.jpeg" alt="Brown Nike Shoe"/>
                         <p><span className="articleBy">SNEAKERS |</span> BY MIKE DESTEFANO</p>
-                        <a href="https://www.complex.com/sneakers/2020/06/weekend-sneaker-release-guide-6-16-20/"><p>A Complete Guide to This Weekend's Sneaker Releases</p></a>
+                        <a href="https://www.complex.com/sneakers/2020/06/weekend-sneaker-release-guide-6-16-20/"><p className="descriptionP">A Complete Guide to This Weekend's Sneaker Releases</p></a>
                     </div>
                     <div className="gravityBox">
                         <img className="gravityShoe" src="../imgs/gravityShoe.jpg" alt="Nike Gravity Shoe"/>
