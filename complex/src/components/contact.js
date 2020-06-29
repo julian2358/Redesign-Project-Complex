91b8c428d5b28cb9b643b26c81135fc6ea43afbf
Josh McNeill <joshmcneill@macbook-pro.attlocal.net>
2020-06-29 18:58:49 -0400

    fixed music modal

--- complex/src/components/contact.js
+++ complex/src/components/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import '../css/contact.css';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
@@ -47,14 +48,23 @@ const defaultState = {
   
     handleSubmit = event => {
       event.preventDefault();
+      console.log(this.state)
       const isValid = this.validate();
       if (isValid) {
         console.log(this.state);
         //Clearing form
       this.setState(defaultState);
+      axios.post(`https://jsonplaceholder.typicode.com/users`, this.state)
+      .then(response => {
+          console.log(response)
+      })
+      .catch(error => {
+
+      })
       }
     };
     render() {
+        const { name, email, body} = this.state
       return (
         <div>
           <Nav />
@@ -69,7 +79,7 @@ const defaultState = {
                     type="text"
                     name="name"
                     placeholder="name"
-                    value={this.state.name}
+                    value={name}
                     onChange={this.handleChange}
                   />
                 </div>
@@ -81,7 +91,7 @@ const defaultState = {
                     type="text"
                     name="email"
                     placeholder="email"
-                    value={this.state.email}
+                    value={email}
                     onChange={this.handleChange}
                   />
                 </div>
@@ -91,6 +101,8 @@ const defaultState = {
                   <textarea
                     placeholder="We would love to hear your feedback!"
                     name="message"
+                    value={body}
+                    onChange={this.handleChange}
                   ></textarea>
                 </div>
   
